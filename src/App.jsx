c753070db121d8b3620a9472c5e29a00183190c3
Willlasbury <asburyw229@gmail.com>
2023-10-23 11:20:42 -0700

    moved notifications to its own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,71 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 
 import blc from "../src/utils/connection";
-import getChar from "../src/utils/characteristics"
+import getChar from "../src/utils/characteristics";
+import notify from "./utils/notify";
 
 export default function Home() {
-    const [server, setServer] = useState()
-    const [sendChar, setSendChar] = useState();
-    const [recienveChar, setRecienveChar] = useState();
+  const [server, setServer] = useState();
+  const [sendChar, setSendChar] = useState();
+  const [recieveChar, setrecieveChar] = useState();
 
+  // initiate the connection and obtain the server
+  async function connect() {
+    const server = await blc();
+    setServer(server);
+    // recieve and save the chatacteristics
+    const { send, recieve } = await getChar(server);
+    setSendChar(send);
+    setrecieveChar(recieve);
+    // start the notifications
+    notify(recieveChar)
+  }
 
-    async function connect () {
-        const server = await blc()
-        setServer(server)
-    }
+  // start notifications once you have obtained the recieve characterisitic
+  // useEffect(() => {
+  //   console.log("receive start");
+  //   async function startNotify() {
+  //     await notify(recieveChar);
+  //   }
+  //   recieveChar && startNotify();
+  // }, recieveChar);
 
-    async function doSomething () {
-      const {send, recieve} = await getChar(server)
-      setSendChar(send)
-      setRecienveChar(recieve)
-    }
-    // TODO: customize notify event to handle personal uses 
-    // TODO: move notify funcionality to its own folder
-    function notify (event) {
-      try {
-        var value = event.target.value;
-        var len = value.byteLength;
-        var bytes = [];
-        for (var k = 0; k < len; k++)
-        {
-            var b = value.getUint8(k);
-            bytes.push(b);
-        }
-        console.log("Data: ", bytes);
+  const doSomething = () => {
+    console.log(
+      "server, sendChar, recieveChar:",
+      server,
+      sendChar,
+      recieveChar
+    );
+  };
 
-    } catch (ex) {
-        alert("ERROR: " + ex.message);
-    }
-    }
+  // get characteristics once you have the server
+  // useEffect(() => {
+  //   async function getCharacteristics() {
+  //     const { send, recieve } = await getChar(server);
+  //     setSendChar(send);
+  //     setrecieveChar(recieve);
+  //   }
+  //   getCharacteristics();
+  // }, server);
 
-    useEffect(()=>{
-      // console.log("server change:", server)
-      // console.log("startNotifications:", startNotifications)
-    },[server])
   return (
-    <main className='h-screen flex flex-col justify-center items-center'>
-      
-      <button className='border-2 border-white rounded-md p-3 mb-3'onClick={connect}> connect </button>
-      <button className='border-2 border-white rounded-md p-3'onClick={doSomething}> do something </button>
+    <main className="h-screen flex flex-col justify-center items-center">
+      <button
+        className="border-2 border-white rounded-md p-3 mb-3"
+        onClick={connect}
+      >
+        {" "}
+        connect{" "}
+      </button>
+      <button
+        className="border-2 border-white rounded-md p-3"
+        onClick={doSomething}
+      >
+        {" "}
+        do something{" "}
+      </button>
     </main>
   );
 }
--- src/utils/characteristics.js
+++ src/utils/characteristics.js
@@ -22,24 +22,23 @@ export default async function getCharacteristics(server) {
     const notifyUuid = "7e4e1702-1ea6-40c9-9dcc-13d34ffead57";
     // writeUuid is called progressor control point on tindeq api
     const writeUuid = "7e4e1703-1ea6-40c9-9dcc-13d34ffead57";
-
     // use funcitons from web bluetooth aka BluetoothRemoteGATTServer and BluetoothRemoteGATTCharacteristics
     const service = await server.getPrimaryService(primary_service);
     const recieve = await service?.getCharacteristic(notifyUuid)
+    const send = await service?.getCharacteristic(writeUuid)
 
     // TODO: test out diffent file structure to handle notifications
-    recieve.addEventListener("characteristicvaluechanged", notify)
-    await recieve.startNotifications()
+    // recieve.addEventListener("characteristicvaluechanged", notify)
+    // await recieve.startNotifications()
 
     // TODO: abstract away send functionality
-    const sendChar = await service?.getCharacteristic(writeUuid)
-    const start = new Uint8Array([cmds.START_WEIGHT_MEAS])
-    const stop = new Uint8Array([cmds.STOP_WEIGHT_MEAS])
-    await sendChar.writeValueWithResponse(start)
-    setTimeout(async () => {
-      await sendChar.writeValueWithResponse(stop)
-    },5000)
-    
+    // const start = new Uint8Array([cmds.START_WEIGHT_MEAS])
+    // const stop = new Uint8Array([cmds.STOP_WEIGHT_MEAS])
+    // await sendChar.writeValueWithResponse(start)
+    // setTimeout(async () => {
+    //   await sendChar.writeValueWithResponse(stop)
+    // },5000)
+    return {send, recieve}
 
   } catch (error) {
     return new Error(`some error: ${error}`);
