a07dc67e938e966efc7ff8c8ede99bc4da0f84cc
Willlasbury <asburyw229@gmail.com>
2023-11-02 17:00:33 -0700

    added comments notify func

--- src/utils/tindeqApi/notify.js
+++ src/utils/tindeqApi/notify.js
@@ -1,25 +1,18 @@
-import api from "./api"
+import api from "../api"
 
 export default async function (recieveChar) {
-    // TODO: customize notify event to handle personal uses 
     async function notify (event, data) {
       try {
-                // populate data var with tindeq data
-        // TODO: add check on first byte from tindeq to validate it is a weight value (0x01)
-        // TODO: send data to server to be decoded and then do something
+        // TODO: add check on first byte from tindeq to validate what the response is
+
+        // create array of ints from tindeq response
         const value = new Uint8Array(event.target.value.buffer)
-        console.log("value:", value)
-        // var len = value.byteLength;
-        // var bytes = [];
-        // for (var k = 0; k < len; k++)
-        // {
-        //     var b = value.getUint8(k);
-
-        //     bytes.push(b);
-        // }
-
+    
+        // turn array into string so it can be used as the body of the post
         const string = value.join(' ')
+
         const res = await api.post(string)
+        
         console.log("res:", res)
     } catch (ex) {
         alert("ERROR: " + ex.message);
