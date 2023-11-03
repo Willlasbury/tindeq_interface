5940d3388e8a2682b52725da30bf0b845b31abb3
Willlasbury <asburyw229@gmail.com>
2023-11-03 11:31:19 -0700

    created func to store data in local memory

---
+++ src/utils/localMem/write.js
@@ -0,0 +1,10 @@
+export default function writeToLM (data) {
+    try {
+        key = 'weight data'
+        // convert data to string
+        data = JSON.stringify(data)
+        localStorage.setItem(key, data)
+    } catch (error) {
+        return new Error('error occured while saving to local memory: ', error)
+    }
+}
