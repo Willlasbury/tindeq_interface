912fba8c446b094f2657d8650351442d5d9e3b18
Willlasbury <asburyw229@gmail.com>
2023-11-10 10:47:37 -0700

    add check for connection to display weight

--- src/components/displayWeight.jsx
+++ src/components/displayWeight.jsx
@@ -1,5 +1,10 @@
-export default function DisplayWeight ({weight}) {
-    if (weight) {
+export default function DisplayWeight ({weight, connected}) {
+    if (!connected) {
+        return (
+            <div>Not connected</div>
+        )
+    }
+    else if (weight) {
         return (
             <div>{weight}</div>
         )
