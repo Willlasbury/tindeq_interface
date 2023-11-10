c54dc5e2149bf9f71e802cb768ad57fb539709cf
Willlasbury <asburyw229@gmail.com>
2023-11-10 10:27:47 -0700

    added button to tare weight

---
+++ src/components/TareBtn.jsx
@@ -0,0 +1,14 @@
+import sendCommand from "../utils/tindeqApi/send_command"
+
+export default function TareBtn ({sendChar}) {
+    function handleClick () {
+        sendCommand(
+            sendChar, 
+            'TARE_SCALE'
+          )
+    }
+
+    return (
+        <button onClick={handleClick}>Tare Scale</button>
+    )
+}
