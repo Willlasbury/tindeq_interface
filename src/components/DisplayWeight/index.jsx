ae3061c37b54b86cc6b631f9328e35ed76351cb5
Willlasbury <asburyw229@gmail.com>
2023-11-13 13:19:26 -0700

    formatted weight display to show single digit

--- src/components/DisplayWeight/index.jsx
+++ src/components/DisplayWeight/index.jsx
@@ -1,4 +1,10 @@
+import styles from './styles.css'
+
 export default function DisplayWeight ({weight, connected}) {
+    function formatWeight (weight) {
+        const num = parseFloat(weight) << 1
+        return num
+    }
     if (!connected) {
         return (
             <div>Not connected</div>
@@ -6,7 +12,7 @@ export default function DisplayWeight ({weight, connected}) {
     }
     else if (weight) {
         return (
-            <div>{weight}</div>
+            <div>{formatWeight(weight)}</div>
         )
     }
     else {
