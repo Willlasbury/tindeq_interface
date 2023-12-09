364247647958b611c08803e87377bf0e14c40a75
Willlasbury <asburyw229@gmail.com>
2023-12-09 16:43:33 -0700

    adjust buttons and bar graph

--- src/components/GraphCurrent/index.jsx
+++ src/components/GraphCurrent/index.jsx
@@ -1,34 +1,50 @@
 import "./styles.css";
-import { BarChart, Bar, CartesianGrid, XAxis, YAxis, ReferenceLine, ResponsiveContainer } from "recharts";
+
+import {
+  BarChart,
+  Bar,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  ReferenceLine,
+  ResponsiveContainer,
+} from "recharts";
 
 export default function BarGraph({ weight, maxWeight }) {
   const data = [
     {
       weight: weight,
-    }
+    },
   ];
   return (
-    <ResponsiveContainer width='100%' height='100%' minHeight={300} minWidth={350}>
-
-    <BarChart
-      data={data}
-      margin={{
-        top: 5,
-        right: 50,
-        left: 0,
-        bottom: 5,
-      }}
+    <ResponsiveContainer
+      width="100%"
+      height="100%"
+      minHeight={300}
+      minWidth={350}
+    >
+      <BarChart
+        data={data}
+        margin={{
+          top: 5,
+          right: 50,
+          left: 0,
+          bottom: 5,
+        }}
       >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" label={weight}/>
-      <YAxis type="number" domain={[0,(Math.ceil((maxWeight)/10)*10)||10]}/>
-      <ReferenceLine y={maxWeight} stroke="red" label={{position:'right', value:`${maxWeight}`}}/>
-      <Bar
-        dataKey="weight"
-        fill="maroon"
-        animationEasing="linear"
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" label={weight} />
+        <YAxis
+          type="number"
+          domain={[0, Math.ceil(maxWeight / 10) * 10 || 10]}
         />
-    </BarChart>
-        </ResponsiveContainer>
+        <ReferenceLine
+          y={maxWeight}
+          stroke="red"
+          label={{ position: "right", value: `${maxWeight}` }}
+        />
+        <Bar dataKey="weight" fill="maroon" animationEasing="linear" />
+      </BarChart>
+    </ResponsiveContainer>
   );
 }
