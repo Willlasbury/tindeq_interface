3d1eef016b72d7901bf941227f2b4f540cc90ed1
Willlasbury <asburyw229@gmail.com>
2024-02-22 14:52:14 -0700

    added complete form options

--- src/components/FingerForm/index.jsx
+++ src/components/FingerForm/index.jsx
@@ -1,42 +1,74 @@
-import { useState } from "react";
+import "./styles.css";
+
+import { useState, useId } from "react";
+import FingerCheckBox from "./FingerCheckBox";
 
 export default function FingerForm() {
   const [formData, setFormData] = useState({
-    hand: "",
-    edge: "",
+    hand: "left",
+    edge: 20,
     grip: "",
     index: true,
     middle: true,
     ring: true,
     pinky: true,
   });
-  console.log("formData:", formData)
   const handleChange = (event) => {
-    const {name, value} = event.target
-    setFormData((prev)=> ({...prev, [name]:value}))
-  }
-  return (
-    <form>
-      <label htmlFor="hand">Hand:</label>
-      <input
-        type="text"
-        id="hand"
-        name="hand"
-        value={formData.hand}
-        onChange={handleChange}
-      />
+    setFormData((prev) => {
+      const { name, value, checked } = event.target;
+      if (["index", "middle", "ring", "pinky"].includes(name)) {
+        return { ...prev, [name]: checked };
+      } else {
+        return { ...prev, [name]: value };
+      }
+    });
+  };
 
-      <label htmlFor="edge">Edge size:</label>
-      <input
-        type="edge"
-        id="edge"
-        name="edge"
-        value={formData.edge}
-        onChange={handleChange}
-      />
-      <label htmlFor="fingers">Fingers</label>
-      <input type="checkbox" />
-      <button type="submit">Submit</button>
-    </form>
+  const func = () => {
+    console.log("formData:", formData);
+  };
+  const edgeSizes = [20, 15, 12, 10, 8, 7, 6, 4];
+  const gripType = ['open', 'half', 'full'];
+  return (
+    <>
+      <form id="finger-form">
+        <label htmlFor="hand">Hand:</label>
+        <select id="hand">
+          <option value="left">left</option>
+          <option value="right">right</option>
+        </select>
+
+        <label htmlFor="edge">Edge size:</label>
+        <select id="edge">
+          {edgeSizes.map((val) => {
+            return (
+              <option key={val} value={val}>
+                {val}
+              </option>
+            );
+          })}
+        </select>
+
+        <label htmlFor="grip">Grip: </label>
+        <select id="grip">
+          {gripType.map((val) => {
+            return (
+              <option key={val} value={val}>
+                {val}
+              </option>
+            );
+          })}
+        </select>
+
+        <ul id="finger-options">
+          <FingerCheckBox finger="index" handleChange={handleChange} />
+          <FingerCheckBox finger="middle" handleChange={handleChange} />
+          <FingerCheckBox finger="ring" handleChange={handleChange} />
+          <FingerCheckBox finger="pinky" handleChange={handleChange} />
+        </ul>
+        <button type="submit">Submit</button>
+      </form>
+      <button onClick={func}>check form data</button>
+    </>
   );
 }
