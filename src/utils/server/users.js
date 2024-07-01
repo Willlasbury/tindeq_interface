87e890851388392f5f0f4e8e5378702f96f9f570
Willlasbury <asburyw229@gmail.com>
2024-07-01 13:22:15 -0600

    add debug to long

--- src/utils/server/users.js
+++ src/utils/server/users.js
@@ -71,6 +71,7 @@ const usersApi = {
         email: email,
         password: password,
       };  
+      console.log("URL_PREFIX:", URL_PREFIX)
       const res = await fetch(`${URL_PREFIX}/users/login`, {
         method: "POST",
         body: JSON.stringify(body),
